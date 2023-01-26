8560e854a8bbe007923c1412c5edb1b05139f58e
Ali Nurdin <alinurdin159@gmail.com>
2023-01-27 05:11:26 +0700

    add logout btn

--- resources/views/vendor/Chatify/layouts/info.blade.php
+++ resources/views/vendor/Chatify/layouts/info.blade.php
@@ -2,6 +2,15 @@
 <div class="avatar av-l chatify-d-flex"></div>
 <p class="info-name">{{ config('chatify.name') }}</p>
 <div class="messenger-infoView-btns">
+    <a class="default" href="{{ route('logout') }}"
+        onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">
+        {{ __('Logout') }}
+    </a>
+
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+        @csrf
+    </form>
     {{-- <a href="#" class="default"><i class="fas fa-camera"></i> default</a> --}}
     <a href="#" class="danger delete-conversation"><i class="fas fa-trash-alt"></i> Delete Conversation</a>
 </div>
